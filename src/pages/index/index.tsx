3fc1b4caeae7bf7c4f16288b64dd80c3cadebbe0
Glinfen <gilinfen@gmail.com>
2024-03-08 22:30:04 +0800

    feat: 🎸 初始化完成

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,11 @@
+import { View } from "@tarojs/components";
+import { useLoad } from "@tarojs/taro";
+import "./index.scss";
+
+export default function Index() {
+  useLoad(() => {
+    console.log("Page loaded.");
+  });
+
+  return <View className="text-[#acc855] text-[80px]">Hello world!</View>;
+}
