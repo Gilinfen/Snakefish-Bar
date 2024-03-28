e8b9fd8ef28afc5612b08040b090eb6b8477992b
Glinfen <gilinfen@gmail.com>
2024-03-28 15:54:27 +0800

    feat: 🎸 heiyuba-v1.0.0-bate

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,5 +1,6 @@
-import { View } from "@tarojs/components";
+import { View, Image } from "@tarojs/components";
 import { useLoad } from "@tarojs/taro";
+import Logo from "public/logo.jpg";
 import "./index.scss";
 
 export default function Index() {
@@ -7,5 +8,9 @@ export default function Index() {
     console.log("Page loaded.");
   });
 
-  return <View className="text-[#acc855] text-[80px]">Hello world!</View>;
+  return (
+    <View className="">
+      <Image src={Logo} className=" w-[1024px] h-[1024px] logo" />
+    </View>
+  );
 }
